24659276f03253b612131e7905f465cf279b68c8
multimedia <multimedia@309-41>
2016-04-11 16:34:47 +0900

    뺄셈기능 테스트 코드 추가
    기본적 기능

--- src/main/java/Calc1.java
+++ src/main/java/Calc1.java
@@ -1,4 +1,16 @@
 
 public class Calc1 {
 
+		private int result=0;
+		
+		public void add(int x, int y){
+			result = x+y;
+		}
+		public void sub(int x, int y){
+			result = x-y;
+		}
+		
+		public int GetResult(){
+			return result;
+		}
 }
